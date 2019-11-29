d2fbd83b2037ed13cddc2995cd38a888d833393e
DongONNS <dongcheng_2018@163.com>
2019-11-29 23:39:31 +0800

    重建数组

--- src/main/java/Hot100/medium/RebuiltQueue.java
+++ src/main/java/Hot100/medium/RebuiltQueue.java
@@ -12,6 +12,15 @@ public class RebuiltQueue {
         }
     }
 
+    /**
+     * * 解题思路：先排序再插入
+     * 1.排序规则：按照先H高度降序，K个数升序排序
+     * 2.遍历排序后的数组，根据K插入到K的位置上
+     *
+     * 核心思想：高个子先站好位，矮个子插入到K位置上，前面肯定有K个高个子，矮个子再插到前面也满足K的要求
+     * @param people
+     * @return
+     */
     public static int[][] reconstructQueue(int[][] people) {
 
         Arrays.sort(people, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
