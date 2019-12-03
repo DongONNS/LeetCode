7daf3a723a5de6f8f0a871246fdfc309a27a9fe3
DongONNS <dongcheng_2018@163.com>
2019-12-03 23:57:51 +0800

    最大面积

---
+++ src/main/java/Hot100/medium/MaxArea.java
@@ -0,0 +1,22 @@
+package Hot100.medium;
+
+public class MaxArea {
+    public static void main(String[] args) {
+        int[] arr = {1,8,6,2,5,4,8,3,7};
+        int maxArea = maxArea(arr);
+        System.out.println(maxArea);
+    }
+    public static int maxArea(int[] height) {
+        int maxArea = 0;
+        int l = 0;
+        int r = height.length-1;
+
+        while(l<r){
+            int s = height[l]>=height[r]?height[r]*(r-l):height[l]*(r-l);
+            maxArea = maxArea>=s?maxArea:s;
+            if(height[l]>=height[r])    r--;
+            else    l++;
+        }
+        return maxArea;
+    }
+}
