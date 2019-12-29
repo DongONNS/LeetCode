f52aad9e2836c0472a8b2daf38970b2af02e4574
DongONNS <dongcheng_2018@163.com>
2019-12-29 23:45:50 +0800

    二叉搜索树的数量

---
+++ src/main/java/letecodePractice/tree/NumTrees.java
@@ -0,0 +1,12 @@
+package letecodePractice.tree;
+
+public class NumTrees {
+    public int numTrees(int n) {
+        // Note: we should use long here instead of int, otherwise overflow
+        long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) C;
+    }
+}
