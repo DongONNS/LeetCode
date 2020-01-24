04010fc9df66deea44cfbd98a11922bc16582a46
DongONNS <dongcheng_2018@163.com>
2020-01-24 23:32:58 +0800

    happy new year

---
+++ src/main/java/letecodePractice/HappyNewYear.java
@@ -0,0 +1,8 @@
+package letecodePractice;
+
+public class HappyNewYear {
+    public static void main(String[] args) {
+        //今天不想敲代码，明天吧...... 我是弱鸡......
+        System.out.println("happy new year!");
+    }
+}
