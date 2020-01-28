e11429d53d9a515c130449a9ad43cfe5b2da0afd
DongONNS <dongcheng_2018@163.com>
2020-01-28 23:51:40 +0800

    threesumCloest

--- src/main/java/letecodePractice/ThreeSumClosest.java
+++ src/main/java/letecodePractice/ThreeSumClosest.java
@@ -8,7 +8,7 @@ public class ThreeSumClosest {
         int ans = nums[0] + nums[1] + nums[2];
         for(int i=0;i<nums.length;i++) {
             int start = i+1, end = nums.length - 1;
-            while(start < end) {
+            while(start < end){
                 int sum = nums[start] + nums[end] + nums[i];
                 if(Math.abs(target - sum) < Math.abs(target - ans))
                     ans = sum;
