4fbc053b06b8403d2c8043702ead3c01bf2a9cbd
DongONNS <dongcheng_2018@163.com>
2020-01-31 00:22:02 +0800

    采用快慢指针法

--- src/main/java/letecodePractice/onetoonehundred/RemoveNthFromEnd.java
+++ src/main/java/letecodePractice/onetoonehundred/RemoveNthFromEnd.java
@@ -1,26 +1,37 @@
 package letecodePractice.onetoonehundred;
 
 public class RemoveNthFromEnd {
-//    public ListNode removeNthFromEnd(ListNode head,int n){
-//
-//        return null;
-//    }
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        int length  = 0;
-        ListNode first = head;
-        while (first != null) {
-            length++;
-            first = first.next;
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode start = pre, end = pre;
+        while(n != 0) {
+            start = start.next;
+            n--;
         }
-        length -= n;
-        first = dummy;
-        while (length > 0) {
-            length--;
-            first = first.next;
+        while(start.next != null) {
+            start = start.next;
+            end = end.next;
         }
-        first.next = first.next.next;
-        return dummy.next;
+        end.next = end.next.next;
+        return pre.next;
     }
+//    public ListNode removeNthFromEnd(ListNode head, int n) {
+//        ListNode dummy = new ListNode(0);
+//        dummy.next = head;
+//        int length  = 0;
+//        ListNode first = head;
+//        while (first != null) {
+//            length++;
+//            first = first.next;
+//        }
+//        length -= n;
+//        first = dummy;
+//        while (length > 0) {
+//            length--;
+//            first = first.next;
+//        }
+//        first.next = first.next.next;
+//        return dummy.next;
+//    }
 }
