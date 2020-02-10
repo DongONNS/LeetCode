f6882c7cdc737a48f7844a1a0bf853dbda1bb2a6
DongONNS <dongcheng_2018@163.com>
2020-02-10 23:50:28 +0800

    clone graph

---
+++ src/main/java/letecodePractice/Graph/CloneGraph.java
@@ -0,0 +1,21 @@
+package letecodePractice.Graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CloneGraph {
+    public Node cloneGraph(Node node) {
+        Map<Node, Node> lookup = new HashMap<>();
+        return dfs(node, lookup);
+    }
+
+    private Node dfs(Node node, Map<Node,Node> lookup) {
+        if (node == null) return null;
+        if (lookup.containsKey(node)) return lookup.get(node);
+        Node clone = new Node(node.val, new ArrayList<>());
+        lookup.put(node, clone);
+        for (Node n : node.neighbors)clone.neighbors.add(dfs(n,lookup));
+        return clone;
+    }
+}
