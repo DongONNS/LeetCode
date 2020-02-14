6e833c73b298789ddee23e801b4e9764514c8ab1
DongONNS <dongcheng_2018@163.com>
2020-02-15 00:35:18 +0800

    cycle pow

--- src/main/java/letecodePractice/math/MyPow.java
+++ src/main/java/letecodePractice/math/MyPow.java
@@ -35,3 +35,20 @@ class FastPow{
         return fastPow(x,N);
     }
 }
+
+class cyclePow{
+    public double myPow(double x,int n){
+        long N = n;
+        if(N<0){
+            x = 1/x;
+            N = -N;
+        }
+        double ans = 1;
+        double current_product = x;
+        for(long i = N ;i > 0;i /= 2){
+            if(i % 2 == 1) ans = ans * current_product;
+            current_product = current_product*current_product;
+        }
+        return ans;
+    }
+}
