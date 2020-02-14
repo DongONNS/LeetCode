0daf8dcaa581211f607ed0dbfac1da420a6f3446
DongONNS <dongcheng_2018@163.com>
2020-02-14 23:48:32 +0800

    mypow

---
+++ src/main/java/letecodePractice/math/MyPow.java
@@ -0,0 +1,14 @@
+package letecodePractice.math;
+
+public class MyPow {
+    public double myPow(double x,int n){
+        long N = n;
+        if(N<0>){
+            x = 1/x;
+            N = -N;
+        }
+        double ans = 1;
+        for (int i= 0;i<N;i++)  ans = ans * x;
+        return ans;
+    }
+}
