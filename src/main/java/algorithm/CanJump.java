82cee7af5cf90cd2688687d562da46e45e61c22c
dongcheng2019 <dongcheng_2018@163.com>
2020-02-27 23:44:56 +0800

    获取第k个排列

--- src/main/java/algorithm/CanJump.java
+++ src/main/java/algorithm/CanJump.java
@@ -1,13 +1,34 @@
 package algorithm;
 
 public class CanJump {
-    public boolean canJump(int[] nums){
-        int lastPos = nums.length - 1;
-        for (int i = nums.length-1;i >= 0;i--){
-            if (i + nums[i] >= lastPos){
-                lastPos = i;
+
+    //贪婪法
+//    public boolean canJump(int[] nums){
+//        int lastPos = nums.length - 1;
+//        for (int i = nums.length-1;i >= 0;i--){
+//            if (i + nums[i] >= lastPos){
+//                lastPos = i;
+//            }
+//        }
+//        return lastPos == 0;
+//    }
+
+    //1.回溯法
+    public boolean canJumpFromPosition(int position,int[] nums){
+        if (position == nums.length -1 ){
+            return true;
+        }
+        int furthestJump = Math.min(position + nums[position],nums.length-1);
+        for (int nextPosition = position+1 ;nextPosition <= furthestJump;nextPosition++){
+            if (canJumpFromPosition(nextPosition,nums)){
+                return true;
             }
         }
-        return lastPos == 0;
+        return false;
     }
+    public boolean canJump(int[] nums){
+        return canJumpFromPosition(0,nums);
+    }
+
+
 }
