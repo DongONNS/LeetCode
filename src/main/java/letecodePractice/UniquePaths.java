cb0e9d1e342f641d7d410b096c8daa26277bdf9c
dongcheng2019 <dongcheng_2018@163.com>
2020-02-28 23:48:57 +0800

    uniquepaths

---
+++ src/main/java/letecodePractice/UniquePaths.java
@@ -0,0 +1,16 @@
+package letecodePractice;
+
+import java.util.Arrays;
+
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[] cur = new int[n];
+        Arrays.fill(cur,1);
+        for (int i = 1; i < m;i++){
+            for (int j = 1; j < n; j++){
+                cur[j] += cur[j-1] ;
+            }
+        }
+        return cur[n-1];
+    }
+}
