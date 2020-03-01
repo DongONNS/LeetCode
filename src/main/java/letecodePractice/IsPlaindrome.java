76409fa0cd2c4709b12f4fc006f4a84662baa17a
dongcheng2019 <dongcheng_2018@163.com>
2020-03-01 23:38:51 +0800

    isPlaindrome

---
+++ src/main/java/letecodePractice/IsPlaindrome.java
@@ -0,0 +1,27 @@
+package letecodePractice;
+
+public class IsPlaindrome {
+    //双指针大法好,受我一拜!
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        char[] chars = s.toCharArray();
+        int left = 0, right = chars.length - 1;
+        while (right > left) {
+            if ((chars[left] >= '0' && chars[left] <= '9') || (chars[left] >= 'a' && chars[left] <= 'z')) {
+                if ((chars[right] >= '0' && chars[right] <= '9') || (chars[right] >= 'a' && chars[right] <= 'z')) {
+                    if (chars[left] != chars[right]) {
+                        return false;
+                    } else {
+                        left++;
+                        right--;
+                    }
+                } else {
+                    right--;
+                }
+            } else {
+                left++;
+            }
+        }
+        return true;
+    }
+}
