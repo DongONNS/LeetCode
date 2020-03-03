972f1e0206a7775a4390bb9cd11f2287da55a6a3
dongcheng2019 <dongcheng_2018@163.com>
2020-03-04 00:08:30 +0800

    double pointer

--- src/main/java/TwoSum2.java
+++ src/main/java/TwoSum2.java
@@ -11,11 +11,27 @@ public class TwoSum2 {
         for(int i = 0;i < numbers.length;i++){
             for(int j = i+1;j < numbers.length;j++){
                 if((numbers[i] + numbers[j]) == target){
-                    result[0] = i;
-                    result[1] = j;
+                    result[0] = i+1;
+                    result[1] = j+1;
                     return result;
                 }
             }
+        }
+        return null;
+    }
+
+    //采用双指针法实现
+    public int[] twoSum2(int[] numbers, int target) {
+        int[] result = new int[2];
+        int left = 0,right = numbers.length-1;
+        while(left < right){
+            int tempSum = numbers[left] + numbers[right];
+            if(tempSum == target){
+                result[0] = left + 1;
+                result[1] = right + 1;
+                return result;
+            }else if(tempSum < target)  left++;
+            else right--;
         }
         return null;
     }
