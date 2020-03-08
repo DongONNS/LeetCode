51c227f64d0c9202be6289865a4a634b1c7774b0
dongcheng2019 <dongcheng_2018@163.com>
2020-03-08 21:46:37 +0800

    double pointer

--- src/main/java/letecodePractice/IsHappy.java
+++ src/main/java/letecodePractice/IsHappy.java
@@ -9,15 +9,27 @@ public class IsHappy {
         System.out.println(happy+"");
     }
 
-    /**
-     * 试图通过递归得到结果，但是很容易栈溢出;
-     * @param n
-     * @return
-     */
+//    /**
+//     * 试图通过递归得到结果，但是很容易栈溢出;
+//     * @param n
+//     * @return
+//     */
+//    public static boolean isHappy(int n){
+//        n = power(n);
+//        if (n == 1) return true;
+//        return isHappy(n);
+//    }
+
+
     public static boolean isHappy(int n){
-        n = power(n);
-        if (n == 1) return true;
-        return isHappy(n);
+        int slow = n;
+        int fast = n;
+        do{
+            slow = power(slow);
+            fast = power(fast);
+            fast = power(fast);
+        }while(slow != fast);
+        return slow == 1;
     }
     public static int power(int k){
         int result = 0;
