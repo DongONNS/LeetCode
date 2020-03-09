b57eecea02d3a32342818de9062fc208d6a71a9f
dongcheng2019 <dongcheng_2018@163.com>
2020-03-09 23:57:55 +0800

    count primes with more efficent way

--- src/main/java/letecodePractice/CountPrime.java
+++ src/main/java/letecodePractice/CountPrime.java
@@ -1,5 +1,7 @@
 package letecodePractice;
 
+import java.util.Arrays;
+
 public class CountPrime {
 
     public static void main(String[] args) {
@@ -7,19 +9,36 @@ public class CountPrime {
         System.out.println(result);
     }
 
-    public static int countPrimes(int n) {
-        if (n==0 || n==1)  return 0;
-        if (n==2)  return 1;
+//    /**
+//     * 暴力法，解题效果很差
+//     * @param n
+//     * @return
+//     */
+//    public static int countPrimes(int n) {
+//        if (n==0 || n==1)  return 0;
+//        if (n==2)  return 1;
+//        int count = 0;
+//        for(int i = 2 ;i < n;i++){
+//            if(isPrime(i)) count++;
+//        }
+//        return count;
+//    }
+//    public static boolean isPrime(int n){
+//        for(int i = 2;i < n;i++){
+//            if(n % i == 0 ) return false;
+//        }
+//        return true;
+//    }
+    public static int countPrimes(int n){
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime,true);
+        for(int i = 2;i*i < n;i++)
+            if (isPrime[i])
+                for (int j = i * i;j < n;j++)
+                    isPrime[j] = false;
         int count = 0;
-        for(int i = 2 ;i < n;i++){
-            if(isPrime(i)) count++;
-        }
+        for(int i = 0;i < n;i++)
+            if (isPrime[i]) count++;
         return count;
-    }
-    public static boolean isPrime(int n){
-        for(int i = 2;i < n;i++){
-            if(n % i == 0 ) return false;
-        }
-        return true;
     }
 }
