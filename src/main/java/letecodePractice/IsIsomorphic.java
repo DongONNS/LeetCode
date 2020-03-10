e9c20861e78d5244ce4317205c4e2c896fc30631
dongcheng2019 <dongcheng_2018@163.com>
2020-03-10 23:45:34 +0800

    is Isomorphic

---
+++ src/main/java/letecodePractice/IsIsomorphic.java
@@ -0,0 +1,22 @@
+package letecodePractice;
+
+import java.util.HashMap;
+
+public class IsIsomorphic {
+    public boolean isIsomorphic(String s, String t) {
+        int n = s.length();
+        HashMap<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            char c1 = s.charAt(i);
+            char c2 = s.charAt(i);
+            if (map.containsKey(c1)) {
+                if (map.get(c1) != c2) {
+                    return false;
+                }
+            } else {
+                map.put(c1, c2);
+            }
+        }
+        return true;
+    }
+}
