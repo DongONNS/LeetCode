06ade88caf29484c22246ef631ccd1b1264eaa9b
dongcheng2019 <dongcheng_2018@163.com>
2020-03-16 00:10:07 +0800

    first bad versio with binary search

--- src/main/java/letecodePractice/FIrstBadVersion.java
+++ src/main/java/letecodePractice/FIrstBadVersion.java
@@ -1,18 +1,25 @@
 package letecodePractice;
 
-public class FIrstBadVersion {
-    public class Solution extends VersionControl {
-        public int firstBadVersion(int n) {
-            int left = 1,right = n;
-            int result = 0;
-            while(left <= right){
-                int mid = left+(right-left)/2;
-                if(isBadVersion(mid)){
+public class FIrstBadVersion extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1,right = n;
+        int result = 0;
+        while(left <= right){
+            int mid = left+(right-left)/2;
+            if(isBadVersion(mid)) {
+                if(!isBadVersion(mid-1)){
                     result = mid;
+                    break;
                 }
-                else left = mid + 1;
+                right = mid - 1;
             }
+            else left = mid + 1;
         }
         return result;
+    }
+}
+abstract class VersionControl{
+    public boolean isBadVersion(int n){
+        return false;
     }
 }
