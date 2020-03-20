54efcb851d602671e8fd464e6452ffd4a1673252
dongcheng2019 <dongcheng_2018@163.com>
2020-03-20 10:11:45 +0800

    improve the can jump with greedy algorithm

--- src/main/java/algorithm/CanJump.java
+++ src/main/java/algorithm/CanJump.java
@@ -1,9 +1,23 @@
 package algorithm;
 
 public class CanJump {
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+        boolean result = canJump(nums);
+        System.out.println(result);
+    }
 
-    //贪婪法
-//    public boolean canJump(int[] nums){
+    public static boolean canJump(int[] nums){
+        int k = 0;
+        for (int i = 0;i < nums.length; i++){
+            if (i > k) return false;
+            k = Math.max(k,i+nums[i]);
+        }
+        return true;
+    }
+
+//    //贪婪法，速度较快
+//    public static boolean canJump(int[] nums){
 //        int lastPos = nums.length - 1;
 //        for (int i = nums.length-1;i >= 0;i--){
 //            if (i + nums[i] >= lastPos){
@@ -13,22 +27,22 @@ public class CanJump {
 //        return lastPos == 0;
 //    }
 
-    //1.回溯法
-    public boolean canJumpFromPosition(int position,int[] nums){
-        if (position == nums.length -1 ){
-            return true;
-        }
-        int furthestJump = Math.min(position + nums[position],nums.length-1);
-        for (int nextPosition = position+1 ;nextPosition <= furthestJump;nextPosition++){
-            if (canJumpFromPosition(nextPosition,nums)){
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean canJump(int[] nums){
-        return canJumpFromPosition(0,nums);
-    }
+    //1.回溯法,超时了
+//    public boolean canJumpFromPosition(int position,int[] nums){
+//        if (position == nums.length -1 ){
+//            return true;
+//        }
+//        int furthestJump = Math.min(position + nums[position],nums.length-1);
+//        for (int nextPosition = position+1 ;nextPosition <= furthestJump;nextPosition++){
+//            if (canJumpFromPosition(nextPosition,nums)){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//    public boolean canJump(int[] nums){
+//        return canJumpFromPosition(0,nums);
+//    }
 
 
 }
