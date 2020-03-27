25a6a4ac6bb3b3a9cd0832b10b8d79c3f0340f5c
dongcheng2019 <dongcheng_2018@163.com>
2020-03-27 23:06:03 +0800

    subsets

---
+++ src/main/java/letecodePractice/SubSets.java
@@ -0,0 +1,22 @@
+package letecodePractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubSets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> output = new ArrayList();
+        output.add(new ArrayList<Integer>());
+
+        for (int num : nums) {
+            List<List<Integer>> newSubsets = new ArrayList();
+            for (List<Integer> curr : output) {
+                newSubsets.add(new ArrayList<Integer>(curr){{add(num);}});
+            }
+            for (List<Integer> curr : newSubsets) {
+                output.add(curr);
+            }
+        }
+        return output;
+    }
+}
