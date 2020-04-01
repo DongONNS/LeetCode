864003829ff96b3c062257a863beb64eb5978c84
dongcheng2019 <dongcheng_2018@163.com>
2020-04-01 23:55:58 +0800

    gray code

---
+++ src/main/java/letecodePractice/GrayCode.java
@@ -0,0 +1,19 @@
+package letecodePractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrayCode {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<Integer>() {{
+            add(0);
+        }};
+        int head = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = res.size() - 1; j >= 0; j--)
+                res.add(head + res.get(j));
+            head <<= 1;
+        }
+        return res;
+    }
+}
--- src/main/java/letecodePractice/RemoveDuplicate.java
+++ src/main/java/letecodePractice/RemoveDuplicate.java
@@ -1,29 +1,43 @@
 package letecodePractice;
 
 public class RemoveDuplicate {
-    public int removeDuplicates(int[] nums){
-        int i = 1,count = 1,length = nums.length;
-        //从第二个元素开始逐个元素处理数组
-        while(i < length){
-            if (nums[i] == nums[i-1]){
-                count++;
-                if (count > 2){
-                    this.remElement(nums,i);
-                    i--;
-                    length--;
-                }
-            }else{
-                count = 1;//重写开始元素计数;
-            }
-            i++;
+    public int removeDuplicate(int[] nums){
+        int j = 1,count = 1;//初始化计数器和第二指针
+        //从数组的第二个元素开始处理并且逐个处理元素
+        for (int i = 1;i < nums.length;i++){
+            //如果当前元素是一个重复元素，增加数量
+            if (nums[i] == nums[i-1]) count++;
+            else  count = 1;
+            if (count <= 2) nums[j++] = nums[i];
         }
-        return length;
+        return j;
     }
-    //删除数组中的指定位置元素
-    public int[] remElement(int[] arr,int index){
-        for (int i = index;i < arr.length;i++){
-            arr[i-1] = arr[i];
-        }
-        return arr;
-    }
+
+
+
+//    public int removeDuplicates(int[] nums){
+//        int i = 1,count = 1,length = nums.length;
+//        //从第二个元素开始逐个元素处理数组
+//        while(i < length){
+//            if (nums[i] == nums[i-1]){
+//                count++;
+//                if (count > 2){
+//                    this.remElement(nums,i);
+//                    i--;
+//                    length--;
+//                }
+//            }else{
+//                count = 1;//重写开始元素计数;
+//            }
+//            i++;
+//        }
+//        return length;
+//    }
+//    //删除数组中的指定位置元素
+//    public int[] remElement(int[] arr,int index){
+//        for (int i = index;i < arr.length;i++){
+//            arr[i-1] = arr[i];
+//        }
+//        return arr;
+//    }
 }
